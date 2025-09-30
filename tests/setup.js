67350728d676766646cdf95faf2f2d99750187ab
juunghaa <jun8ha@naver.com>
2025-10-01 00:46:58 +0900

    setup.js

--- tests/setup.js
+++ tests/setup.js
@@ -1,20 +1,39 @@
 // tests/setup.js
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongoose = require('mongoose');
-const { connectDB, closeDB } = require('../src/config/db');
+// const { MongoMemoryServer } = require('mongodb-memory-server');
+// const mongoose = require('mongoose');
+// const { connectDB, closeDB } = require('../src/config/db');
 
-let mongoServer;
+// let mongoServer;
+
+// beforeAll(async () => {
+//   mongoServer = await MongoMemoryServer.create();
+//   const uri = mongoServer.getUri();
+//   await connectDB(uri, 'testdb');   // 교수님 코드의 connectDB 재사용
+// });
+
+// afterAll(async () => {
+//   await closeDB();
+//   if (mongoServer) {
+//     await mongoServer.stop();
+//   }
+//   await mongoose.disconnect();
+// });
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+let mongo;
 
 beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-  await connectDB(uri, 'testdb');   // 교수님 코드의 connectDB 재사용
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 });
 
 afterAll(async () => {
-  await closeDB();
-  if (mongoServer) {
-    await mongoServer.stop();
-  }
-  await mongoose.disconnect();
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongo.stop();
 });
